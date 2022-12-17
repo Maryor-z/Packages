6d76f6f737e804bc1eb0c5761ee38fd5e7b673bb
Maryor-z <88011448+Maryor-z@users.noreply.github.com>
2022-12-17 09:15:09 +0100

    Update Main.java

--- src/dev/maryor/Main.java
+++ src/dev/maryor/Main.java
@@ -1,2 +1,12 @@
-package dev.maryor;public class Main {
+package dev.maryor;
+
+import com.abc.first.Item;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Item firstItem = new Item("Burger");
+        System.out.println(firstItem);
+    }
 }
